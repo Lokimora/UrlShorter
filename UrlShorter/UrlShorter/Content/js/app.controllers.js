9f8a2d9c271c1cdb7c0536f00277bec1c7c396ee
lokimora <lokki.lam@gmail.com>
2016-06-07 11:09:20 +0300

    Url changes

--- UrlShorter/UrlShorter/Content/js/app.controllers.js
+++ UrlShorter/UrlShorter/Content/js/app.controllers.js
@@ -10,7 +10,7 @@
         };
 
 
-        $http.post("http://localhost:54601/reducer/create", content).success(function (response) {
+        $http.post("/reducer/create", content).success(function (response) {
             $scope.message = $location.host() + ":" + $location.port() + "/" + response.ShortRelUrl;
 
         }).error(function (response) {
@@ -22,7 +22,7 @@
 .controller("urlList-controller", ["$scope", "$location", "$http", function ($scope, $location, $http) {
     $scope.links = [];
 
-    $http.get("http://localhost:54601/reducer/getall").success(function (response) {
+    $http.get("/reducer/getall").success(function (response) {
       
         $scope.links = response;
         angular.forEach($scope.links, function (value, key) {
